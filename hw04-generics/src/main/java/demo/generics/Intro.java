1123df4a01234ec193263360b8755d2bb5dc30d5
ArtemKhairov <artemxairoff@yandex.ru>
2022-12-07 21:39:57 +0700

    generics

---
+++ hw04-generics/src/main/java/demo/generics/Intro.java
@@ -0,0 +1,78 @@
+package demo.generics;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author sergey
+ * created on 23.11.18.
+ */
+public class Intro {
+
+    public static void main( String[] args ) {
+        new Intro().beforeGenerics();
+//    new Intro().generics();
+    }
+
+    //До Generics
+    private void beforeGenerics() {
+        List list = new ArrayList();
+        list.add( 4.0 );
+        list.add( 4L );
+        list.add( "Hello" );
+        list.add( LocalTime.now() );
+
+        printRow( list );
+        System.out.println(summ(list));
+    }
+
+    private void printRow( List list ) {
+        for ( Object item : list ) { //Object !!!
+            System.out.println( item );
+        }
+    }
+
+    //Эра Generics
+    private void generics() {
+        List<Integer> list = new ArrayList<>();
+        //list.add(4.0); //ошибка компиляции
+        //list.add(4L);    //ошибка компиляции
+        //list.add("Hello"); //ошибка компиляции
+        //list.add(LocalTime.now()); //ошибка компиляции
+
+        list.add( 2 );
+        list.add( 3 );
+
+        sumInt( list );
+
+        print( list );
+
+    }
+
+    private void sumInt( List<Integer> list ) {
+        int summ = 0;
+        for ( int val : list ) {  //конкретный тип
+            summ += val;
+        }
+        System.out.println( "\nsumma:" + summ );
+    }
+
+    private <T> void print( List<T> list ) {
+        System.out.println( "" );
+        for ( T item : list ) {
+            System.out.println( item );
+        }
+    }
+
+    private Double summ(List list){
+        Double sum = 0.0;
+        for (Object item : list){
+            if(item instanceof Number){
+                sum += Double.valueOf(item.toString());
+            }
+        }
+        return sum;
+    }
+
+}
